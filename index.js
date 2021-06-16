af6fdcc65e30a38005418e76b539d751af47fbeb
Mario Martinez <mario.martinez.olivares@outlook.es>
2021-06-15 22:39:49 -0500

    creada funcion para refrescar de manera correcta a tabla

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var numeroDeEmpleados=0
 //funcion para formatear el salario
 const formatoSalario = (salario)=>{
     const salarioUSD=new window.Intl.NumberFormat("es-ES",{
@@ -15,20 +16,21 @@ const formatoSalarioDolares = (salario)=>{
     return salarioUSD;
 }
 //declaracion del metodo constructor de empleado
-function nuevoEmpleado(nombre,salario,empresa)
+function nuevoEmpleado(id,nombre,salario,empresa)
 {
+    this.id=id;
     this.nombre=nombre;
     this.salario=salario;
     this.empresa=empresa;
 }
 //arreglo que contendra a todos los empleados
 var empleados=[
-    {nombre:"mario",salario:"10000", empresa:"pjmop"},
+    {id:numeroDeEmpleados++,nombre:"mario",salario:"10000", empresa:"pjmop"},
 ];
 //creacion de una fila
 const fila=document.createElement('tr');
-//creacion de una columna
-const columna= document.createElement('td');
+//fila.className="registro"
+
 //captura de los inputs del empleado
 const  inputAgregar= document.querySelector('input.campoIngresar')
 const inputSalario=document.querySelector('input.campoIngresarSal')
@@ -39,27 +41,37 @@ const tabla=document.querySelector('.cuerpotabla');
 const agregar=document.querySelector('.botoningresar');
 //funcion que agrega nuevos empleados
 const funcionAgregar = () =>{
+    funcionLimpiarTabla()
     empleados.push(new nuevoEmpleado(
+    numeroDeEmpleados++,
     inputAgregar.value,
     inputSalario.value,
     inputEmpresa.value));
-    tabla.insertAdjacentElement('afterbegin',fila);
-    tabla.innerHTML += "<td>"+inputAgregar.value+"</td>"+
-    "<td>"+formatoSalario(inputSalario.value)+"</td>"+
-    "<td>"+inputEmpresa.value+"</td>";
+    funcionMostrarEmpleados();
 };
 //evento de escucha de boton agregar
 agregar.addEventListener('click',funcionAgregar);
 
-//funcion para mostrar los empleados base
-const funcionRegistrosBase= () => {
-    let arrEmpleados=empleados.map(item => {
-        tabla.insertAdjacentElement('afterbegin',fila);
-    tabla.innerHTML += "<td>"+item.nombre+"</td>"+
-    "<td>"+formatoSalario(item.salario)+"</td>"+
-    "<td>"+item.empresa+"</td>";
+//funcion para mostrar los empleados
+const funcionMostrarEmpleados= () => {
+    let arrEmpleados=empleados.forEach(item => {
+        //numeroDeEmpleados++;
+        tabla.innerHTML += 
+        `<tr class=empleado${item.id}>`+
+        "<td>"+item.nombre+"</td>"+
+        "<td>"+formatoSalario(item.salario)+"</td>"+
+        "<td>"+item.empresa+"</td>"
+        +"</tr>";
     })
 } 
+//LIMPIAR TABLA
+const funcionLimpiarTabla= () => {
+    for(let i=0;i<numeroDeEmpleados;i++)
+    {
+            let empleado=  document.querySelector(`tr.empleado${i}`)
+            tabla.removeChild(empleado)
+    }
+}
 //Buscar
 //boton buscar
 const buscar=document.querySelector('.botonbuscar');
@@ -68,11 +80,9 @@ const inputBusqueda=document.querySelector('.campobuscar');
 //funcion que busca empleados
 const funcionBuscar = () =>{
     console.log('buscando')
-    var filtrosBusqueda = empleados.filter(function(empleado){
+    var filtrosBusqueda = empleados.filter(empleado =>{
         if(inputBusqueda.value===empleado.nombre)
         return empleado.nombre
-        else if(inputBusqueda.value==empleado.salario)
-        return empleado.salario
         else if(inputBusqueda.value===empleado.empresa)
         return empleado.empresa;
     });
@@ -91,28 +101,35 @@ const inputEditNombre= document.querySelector('.campoeditarnombre');
 const inputEditSalario= document.querySelector('.campoeditarsalario');
 //funcion editar
 const funcionEditar = ()  =>{
-    var registroEditado= empleados.map(item => {
+    var registroEditado= empleados.forEach(item => {
+        funcionLimpiarTabla()
         if(item.nombre === inputNombreActual.value &&
-            item.salario === inputSalarioActual.value)
+            item.salario == inputSalarioActual.value)
             {
                 item.nombre = inputEditNombre.value;
                 item.salario = inputEditSalario.value;
-                console.log(`el nuevo registro es:${item.nombre} ${item.empresa}`)
+                console.log(`el nuevo registro es:${item.nombre} ${item.salario}`)
             }
+            funcionMostrarEmpleados()
     })
 };
 //evento de escucha de actualizar
 editar.addEventListener('click',funcionEditar);
 
-
 //CONVERSION A DOLARES
 const botonConvertirSalario=document.querySelector('.conversionDolares');
 //funcion de converison a dolares
 const funcionConversion  = () => {
+    funcionLimpiarTabla()
     var recorreArray= empleados.forEach(item => {
         let salarioUSD=item.salario/21.50
-        console.log(`Salarios convertidos en dolares ${formatoSalarioDolares(salarioUSD)}`);
-        item.salario=salarioUSD; 
+        item.salario=salarioUSD;
+        tabla.innerHTML += 
+        `<tr class=empleado${item.id}>`+
+        "<td>"+item.nombre+"</td>"+
+        "<td>"+formatoSalarioDolares(item.salario)+"</td>"+
+        "<td>"+item.empresa+"</td>"
+        +"</tr>";
     })
 };
 //EVENTO QUE ESCUCHA A LA CONVERSION A DOLARES 
@@ -123,5 +140,5 @@ const mostrarNoEmpleados= document.querySelector('.noEmpleados');
 const funcionNoEmpleados= () =>{
     mostrarNoEmpleados.innerHTML = `No empleados:${empleados.length}`;
 }
-funcionRegistrosBase();
+funcionMostrarEmpleados();
 setInterval(funcionNoEmpleados,2000);
